daffc533937de654b24b36d4dc91bc47b57806c8
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-13 02:31:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,6 +13,10 @@ class Scraper
     self.get_page.css(".post")
   end
   
+  def make_courses 
+    
+    get_courses
+  
     def print_courses
     self.make_courses
     Course.all.each do |course|
