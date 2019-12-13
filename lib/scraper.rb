46f4cd99c337097b2f2677962c9fa479e2982729
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-12-13 02:37:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
     
     self.get_courses.each do |p|
       course = Course.new
-      course.title = p.css("h2")
+      course.title = p.css("h2").text
   
   
     def print_courses
